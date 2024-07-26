49f5bf6f60816f0c278dde5b93dbf986efc6b55a
Kiran dhoke <dhokekiran98@gmail.com>
2024-07-27 05:08:56 +0530

    commit

--- secrets/secrets.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Keep your Stripe API key protected by including it as an environment variable
-// or in a private script that does not publicly expose the source code.
-
-// This is a public sample test API key.
-// Don’t submit any personally identifiable information in requests made with this key.
-// Sign in to see your own test API key embedded in code samples.
-$stripeSecretKey = 'pk_test_51KW8A8ItvA6u4On8YM6AE95ytcqBr0LeRBEEFw4f5MDQPTDBWVt2TAWXhfynCn7NUVwaGc2eP431DNCkmfMaB0AF00firEVJ58 ';
